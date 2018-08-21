1afa4cb1c6fccbdcf61ff98213dfe067475ddf35
Michael Tyson <michael@atastypixel.com>
2018-08-21 18:01:48 +1000

    Removed metering from AESplitterModule

--- TheAmazingAudioEngine/Modules/Generation/AESplitterModule.h
+++ TheAmazingAudioEngine/Modules/Generation/AESplitterModule.h
@@ -56,12 +56,6 @@ extern "C" {
 //! You should not change this value while the module is in use.
 @property (nonatomic) int numberOfChannels;
 
-//! Average level
-@property (nonatomic) double average;
-
-//! Peak level
-@property (nonatomic) double peak;
-
 @end
 
 #ifdef __cplusplus
--- TheAmazingAudioEngine/Modules/Generation/AESplitterModule.m
+++ TheAmazingAudioEngine/Modules/Generation/AESplitterModule.m
@@ -31,22 +31,16 @@
 #import "AEDSPUtilities.h"
 #import <Accelerate/Accelerate.h>
 
-static const float kAvgFalloffPerAnalysis = 0.1;
-static const float kPeakFalloffPerAnalysis = 0.01;
-
 @interface AESplitterModule () {
     AudioBufferList * _buffer;
     AudioTimeStamp _timestamp;
     UInt64 _bufferedTime;
     UInt32 _bufferedFrames;
-    float _average;
-    float _peak;
 }
 @property (nonatomic, strong, readwrite) AEModule * module;
 @end
 
 @implementation AESplitterModule
-@dynamic average, peak;
 
 - (instancetype)initWithRenderer:(AERenderer *)renderer module:(AEModule *)module {
     if ( !(self = [super initWithRenderer:renderer]) ) return nil;
@@ -64,15 +58,6 @@ static const float kPeakFalloffPerAnalysis = 0.01;
     _buffer = AEAudioBufferListCreateWithFormat(AEAudioDescriptionWithChannelsAndRate(_numberOfChannels, 0),
                                                 AEBufferStackMaxFramesPerSlice);
 }
-
-- (double)peak {
-    return AEDSPRatioToDecibels(_peak);
-}
-
-- (double)average {
-    return AEDSPRatioToDecibels(_average);
-}
-
 
 static void AESplitterModuleProcess(__unsafe_unretained AESplitterModule * THIS, const AERenderContext * _Nonnull context) {
     
@@ -97,18 +82,9 @@ static void AESplitterModuleProcess(__unsafe_unretained AESplitterModule * THIS,
         // Perform analysis
         const AudioBufferList * buffer = AEBufferStackGet(context->stack, 0);
         if ( buffer ) {
-            float max = 0;
-            for ( int i=0; i<buffer->mNumberBuffers; i++ ) {
-                vDSP_maxmgv((float*)buffer->mBuffers[i].mData, 1, &max, context->frames);
-            }
-            THIS->_average = (kAvgFalloffPerAnalysis * max) + ((1.0-kAvgFalloffPerAnalysis) * THIS->_average);
-            THIS->_peak = MAX(max, ((1.0-kPeakFalloffPerAnalysis) * THIS->_peak));
-            
-            
             THIS->_timestamp = *AEBufferStackGetTimeStampForBuffer(context->stack, 0);
             AEAudioBufferListCopyContents(THIS->_buffer, AEBufferStackGet(context->stack, 0), 0, 0, context->frames);
         } else {
-            THIS->_average = THIS->_peak = 0;
             THIS->_timestamp = *context->timestamp;
             AEAudioBufferListSilence(THIS->_buffer, 0, context->frames);
         }
